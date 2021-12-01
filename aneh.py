97874c4ea95b0669cb7d08c32cae9c4559d22139
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-12-01 17:24:34 +0700

    Create aneh.py

---
+++ aneh.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import os,sys,time
+
+def menu():
+        os.system("clear")
+        os.system("figlet Aneh?")
+        print "\t \033[36;1mTools \033[35;1mBy \033[33;1mReza \033[32;1mAlfauzan\n"
+        print("\033[36;1m1. Send Malware")
+        print("\033[36;1m2. Ubah Tampilan")
+        print("\033[36;1m3. Spam-no Work")
+        print("\033[36;1m4. Perkalian")
+        print("\033[36;1m5. Exit")
+        print("\033[36;1m6. info SC")
+        # input
+        pilih = input("\033[35;1m==> \033[36;1mMasukkan \033[32;1mpilihan : ")
+        if pilih ==1:
+                os.system("clear")
+                os.system("figler Wait..")
+                os.system("git clone https://github.com/GRCR4K3R/Marware")
+        if pilih ==2:
+                os.system("clear")
+                os.system("figler Wait..")
+                os.system("git clone https://github.com/GRCR4K3R/Tampilan")
+        if pilih ==3:
+                os.system("clear")
+                os.system("figler Wait..")
+                os.system("git clone https://github.com/GRCR4K3R/spam-pesan")
+        if pilih ==4:
+                os.system("clear")
+                os.system("figler Wait..")
+                os.system("git clone https://github.com/GRCR4K3R/Perkalian")
+        if pilih ==5:
+                os.system("clear")
+                os.system("figler Exit")
+                os.system("figlet Good Byee")
+        if pilih 6==:
+                os.system("clear")
+                os.system("figlet Info")
+                logo = """
+                \033[32;1m======================================================
+                \033[32;1m=+=>(+) \033[36;1mAuthor : Reza Alfauzan                 \033[32;1m(+)<=+=
+                \033[31;1m=+=>(+) \033[36;1mGithub : GRCR4K3R                      \033[34;1m(+)<=+=
+                \033[36;1m=+=>(+) \033[32;1mJangan recode bang entar error         \033[36;1m(+)<=+=
+                \033[36;1m=+=>(+) \033[36;1mMaaf Ya Gua masih noob:)               (+)<=+=
+                ======================================================
+                \033[36;1m**********************\033[32;1m*****************\033[36;1m***************"""
+                print"\t \033[36;1mTools \033[35;1mBy \033[33;1mReza \033[32;1mAlfauzan\n"
+                print logo
+menu()
